b35e1abec0f02c5bfc3e25c62ef8245c0565567f
longxinwu <1043547002@qq.com>
2021-02-23 07:59:28 +0800

    1 网络编程学习

---
+++ src/main/java/com/lonxinwu/thread/ThreadEchoHandler.java
@@ -0,0 +1,37 @@
+package com.lonxinwu.thread;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ThreadEchoHandler implements Runnable{
+    private Socket incoming;
+    public ThreadEchoHandler(Socket i){
+        incoming = i;
+    }
+    public void run(){
+        try{
+            try{
+                InputStream inStream = incoming.getInputStream();
+                OutputStream outStream = incoming.getOutputStream();
+                Scanner in = new Scanner(inStream);
+                PrintWriter out = new PrintWriter(outStream, true);
+                out.println("Hello! Enter BYE to Exit");
+                boolean done = false;
+                while(!done && in.hasNextLine()){
+                    String line = in.nextLine();
+                    out.println("Echo: " + line);
+                    if(line.trim().equals("BYE")){
+                        done = true;
+                    }
+                }
+            }finally {
+                incoming.close();
+            }
+        }catch (Exception e){
+            System.out.println("error" + e);
+        }
+    }
+}
--- src/main/java/com/lonxinwu/thread/ThreadTest.java
+++ src/main/java/com/lonxinwu/thread/ThreadTest.java
@@ -1,5 +1,8 @@
 package com.lonxinwu.thread;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.concurrent.*;
 
 /**
@@ -7,7 +10,7 @@ import java.util.concurrent.*;
  */
 public class ThreadTest {
     public static void main(String[] args){
-        ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 200, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(5));
+        /*ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 200, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(5));
         // ExecutorService executor = Executors.newCachedThreadPool();
         for(int i=0;i<15;i++){
             MyTask myTask = new MyTask(i);
@@ -15,7 +18,21 @@ public class ThreadTest {
             System.out.println("线程池中线程数目："+executor.getPoolSize()+"，队列中等待执行的任务数目："+
                     executor.getQueue().size()+"，已执行玩别的任务数目："+executor.getCompletedTaskCount());
         }
-        executor.shutdown();
+        executor.shutdown();*/
+        try{
+            int i =1;
+            ServerSocket socket = new ServerSocket(8189);
+            while(true){
+                Socket incoming = socket.accept();
+                System.out.println("Socket" + i);
+                Runnable r = new ThreadEchoHandler(incoming);
+                Thread t = new Thread(r);
+                t.start();
+                i++;
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
 }
